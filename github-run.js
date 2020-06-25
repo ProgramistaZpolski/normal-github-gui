c00b1d3b5bcf70eea052c4fcac58a3b6d538d231
TheTrollErrorsGuy65 <35165895+TheTrollErrorsGuy65@users.noreply.github.com>
2020-06-25 20:06:06 +0200

    v1.1
    yay

--- github-run.js
+++ github-run.js
@@ -3,9 +3,11 @@ function normalgithub_run() {
     if(document.getElementById("normalgithub-stats") !== null) {
         document.body.removeChild(document.getElementById("normalgithub-stats"))
     }
+    let dv;
     setTimeout(function() {
         if(document.getElementsByClassName("css-truncate css-truncate-overflow text-gray").length !== 0) {
-            let a = document.getElementsByClassName("css-truncate css-truncate-overflow text-gray")[0].getElementsByTagName("a")[0]
+            let a = document.getElementsByClassName("css-truncate css-truncate-overflow text-gray")[0].getElementsByTagName("a")[0];
+
             let author = a.outerHTML;
             a.innerHTML = a.innerHTML.replace(a.innerHTML, "").replace("committed", "")
             document.getElementsByClassName("pl-5 mt-2 flex-order-1 width-full")[0].innerHTML = author + document.getElementsByClassName("pl-5 mt-2 flex-order-1 width-full")[0].innerHTML
@@ -35,7 +37,7 @@ function normalgithub_run() {
             }
             for(let svg of document.getElementsByTagName("svg")) {
                 if(svg.getAttribute("aria-label") == "Edit repository metadata") {
-                    // found an edit button!
+                    // found a edit button!
                     let a = svg.parentNode.parentNode.cloneNode(true);
                     a.onclick = function() {
                         de.style.visibility = "visible"
@@ -43,7 +45,7 @@ function normalgithub_run() {
                     a.innerHTML = "Edit"
                     a.style.cursor = "pointer"
                     a.style.position = "relative"
-                    a.style.top = "5px"
+                    a.style.top = "-40px"
                     a.style.right = "9px"
                     a.style.padding = "5px 9px"
                     a.style.background = "linear-gradient(to bottom, #fafbfc, #f0f3f6)"
@@ -84,7 +86,7 @@ function normalgithub_run() {
         hi.style.borderRadius = "2px"
     }
     let css = document.createElement("style")
-    css.innerHTML = `.UnderlineNav-item.selected, .UnderlineNav-item[aria-current]:not([aria-current=false]) {background-color: white !important;border-color: transparent !important;border: 1px #e1e4e8 solid !important;border-top: 3px #e16227 solid !important;border-bottom-color: transparent !important;}.avatar-user{border-radius: 2.5px !important;}nav[aria-label="User profile"]{margin-left: 170px !important;}rect{border-radius: 0px !important}.new-user-avatar-cta{margin-top: 10px;border-radius: 2px;}.social-count{border-radius: 2px;}.octicon-kebab-horizontal{/*kebab mmmm*/display: none;}.octicon-download{display: none;}.octicon-pencil{fill: black;}.news{width: calc(100% - 400px);}a[href="/new"] {color: black !important;}a[href="/new"] svg {fill: black !important;}.dropdown-menu{border-radius: 2px !important;}.repository-content {width: 990px;margin-top: 90px;position: absolute;top: initial;left: 50%;transform: translateX(-50%);margin-bottom: 150px}.flash-full{display: none;}.repohead h1 {max-height: 32px;}.State {border-radius: 2px;}#blob-more-options-details{display: none;}.Box-header{padding: 9px 16px;height: 40px;}.Box-row {border-bottom: rgb(200,200,200) 1px solid;}.Box-row:last-of-type {border-bottom: none !important;}.btn-mktg{border-radius: 2px !important;}.day{rx: 0px !important;ry: 0px !important;}.css-truncate.css-truncate-overflow.text-gray{position: absolute;right: 10px;background-color: #f1f8fe !important;}.pl-5.mt-2.flex-order-1.width-full{display: block !important;height: 0px !important;}.pl-5.mt-2.flex-order-1.width-full pre {visibility: hidden !important;.ellipsis-expander.js-details-target{display: none !important;}.hidden-text-expander.d-inline-block {visibility: hidden !important;}`
+    css.innerHTML = `.UnderlineNav-item.selected, .UnderlineNav-item[aria-current]:not([aria-current=false]) {background-color: white !important;border-color: transparent !important;border: 1px #e1e4e8 solid !important;border-top: 3px #e16227 solid !important;border-bottom-color: transparent !important;}.avatar-user{border-radius: 2.5px !important;}nav[aria-label="User profile"]{margin-left: 170px !important;}rect{border-radius: 0px !important}.new-user-avatar-cta{margin-top: 10px;border-radius: 2px;}.social-count{border-radius: 2px;}.octicon-kebab-horizontal{/*kebab mmmm*/display: none;}.octicon-download{display: none;}.octicon-pencil{fill: black;}.news{width: calc(100% - 400px);}a[href="/new"] {color: black !important;}a[href="/new"] svg {fill: black !important;}.dropdown-menu{border-radius: 2px !important;}.repository-content {width: 990px;margin-top: 100px;position: absolute;top: initial;left: 50%;transform: translateX(-50%);margin-bottom: 150px}.repohead h1 {max-height: 32px;}.State {border-radius: 2px;}#blob-more-options-details{display: none;}.Box-header{padding: 9px 16px;height: 40px;}.Box-row {border-bottom: rgb(200,200,200) 1px solid;}.Box-row:last-of-type {border-bottom: none !important;}.btn-mktg{border-radius: 2px !important;}.day{rx: 0px !important;ry: 0px !important;}.css-truncate.css-truncate-overflow.text-gray{position: absolute;right: 10px;background-color: #f1f8fe !important;}.pl-5.mt-2.flex-order-1.width-full{display: block !important;height: 0px !important;}.pl-5.mt-2.flex-order-1.width-full pre {visibility: hidden !important;.ellipsis-expander.js-details-target{display: none !important;}.hidden-text-expander.d-inline-block {visibility: hidden !important;}`
     if(window.innerWidth <= 1325) {
         css.innerHTML += `aside[aria-label="Explore"]{visibility: hidden;}`
     }
@@ -94,10 +96,11 @@ function normalgithub_run() {
         let diff = window.innerWidth - 1024
         rh.style.paddingLeft = diff / 2 + "px";
         rh.style.paddingRight = diff / 2 + "px";
-        css.innerHTML += ".repohead{padding-left: " + diff / 2 + "px;padding-right: " + diff / 2 + "px;}"
+        rh.style.maxHeight = "114.25px"
+        css.innerHTML += ".repohead{padding-left: " + diff / 2 + "px;padding-right: " + diff / 2 + "px;max-height: 113.25px !important;}"
     }
     catch(error){
-        console.error("Normal GitHub error: " + error)
+        
     }
     try {
         document.getElementsByClassName("flex-order-1 flex-md-order-none mt-2 mt-md-0")[0].style.display = "none"
@@ -115,11 +118,11 @@ function normalgithub_run() {
                 d.id = "normalgithub-stats"
                 d.style.border = "1px #c4c8cc solid"
                 d.innerHTML = i.parentNode.innerHTML;
-                d.style.position = "absolute"
-                document.body.appendChild(d)
+                d.style.position = "relative"
+                document.getElementsByClassName("repohead")[0].appendChild(d)
                 d.style.left = "50%"
                 d.style.transform = "translateX(-50%)"
-                d.style.top = "230px"
+                d.style.top = "70px"
                 d.style.paddingTop = "15px"
                 d.style.paddingBottom = "5px"
                 
@@ -127,13 +130,23 @@ function normalgithub_run() {
                 lgs.style.width = "989px"
                 lgs.style.borderRadius = "0px"
                 lgs.style.position = "relative"
+                for(let mtl of document.getElementsByClassName("muted-link")) {
+                    if(mtl.href.includes("#readme") == true || mtl.href.includes("/LICENSE") == true) {
+                        d.getElementsByClassName("list-style-none d-flex")[0].appendChild(mtl)
+                    }
+                }
+                let addNumber = 120
+                let a_els = d.getElementsByClassName("list-style-none d-flex")[0].getElementsByTagName("a")
+                a_els[a_els.length - 1].parentNode.removeChild(a_els[a_els.length - 1])
+                a_els = d.getElementsByClassName("list-style-none d-flex")[0].getElementsByTagName("a");
+                if(a_els.length == 3) addNumber = 320
                 lgs.style.top = "5px"
                 let leftpx = 10
                 for(let a of d.getElementsByTagName("a")) {
                     a.style.position = "relative"
                     a.style.top = "-5px"
                     a.style.left = leftpx + "px"
-                    leftpx += 320
+                    leftpx += addNumber
                 }
                 i.parentNode.style.display = "none"
             }
@@ -151,19 +164,23 @@ function normalgithub_run() {
         let del = document.createElement("p")
         del.style.position = "absolute"
         del.id = "description"
-        del.style.top = "175px";
+        del.style.top = "120px";
         del.style.left = "calc(50% - 20px)";
         del.style.width = "950px"
-        del.innerHTML = desc;
-        document.body.appendChild(del)
-        if(document.getElementsByClassName("fork-flag mt-1").length !== 0) {
-            del.style.top = "185px"
-            document.getElementById("normalgithub-stats").style.top = "240px"
+        if(desc.length > 140) {
+            let full = ""
+            full = desc
+            desc = desc.substring(1,140) + "..."
+            del.onclick = function() {
+                alert(full);
+            }
         }
+        del.innerHTML = desc;
+        document.getElementsByClassName("repohead")[0].appendChild(del)
         del.style.transform = "translateX(-50%)";
         let tpcs = document.getElementsByClassName("list-topics-container")[0].cloneNode(true)
         tpcs.style.position = "absolute"
-        tpcs.style.top = "200px"
+        tpcs.style.top = "155px"
         tpcs.style.left = "50%"
         tpcs.style.width = "990px"
         tpcs.style.height = "28px";
@@ -175,7 +192,7 @@ function normalgithub_run() {
         if(document.getElementsByClassName("fork-flag mt-1").length !== 0) {
             tpcs.style.top = "210px"
         }
-        document.body.appendChild(tpcs)
+        document.getElementsByClassName("repohead")[0].appendChild(tpcs)
     }
     catch(error){//css.innerHTML += ".repository-content {margin-top: 20px !important;}"
     }
@@ -183,15 +200,20 @@ function normalgithub_run() {
 }
 
 
-let oldurl = window.location.pathname
-// github uses this weird thing like youtube which doesn't load the new page entirely when you click on a link
+let oldurl = window.location.href
+let dbce = false;
+
 let urlchange = setInterval(function() {
-    if(window.location.pathname !== oldurl) {
-        setTimeout(function() {
-            window.location = window.location.pathname;
-        }, 10)
+    if(dbce == false) {
+        if(window.location.href !== oldurl) {
+            window.location = window.location.pathname
+            dbce = true
+            setTimeout(function() {
+    
+            }, 2000)
+        }
+        oldurl = window.location.href
     }
-    oldurl = window.location.pathname
-}, 1000)
+}, 100)
 
 normalgithub_run();
